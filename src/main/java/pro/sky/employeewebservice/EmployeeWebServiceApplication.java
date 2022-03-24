13b624a79b75add377b3e796f9c1a4526b1268bb
auvararaksin <auvaraksin@mail.ru>
2022-03-24 21:59:56 +0200

    Initial commit

---
+++ src/main/java/pro/sky/employeewebservice/EmployeeWebServiceApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/employeewebservice/EmployeeWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
